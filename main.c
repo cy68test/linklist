a6872d9e01f78b0c4b81153653cfc06135c0ab22
Ubuntu <cy68@dingxiang191.mo0yiaf2v4aezp0gtchu2dwhkb.xx.internal.cloudapp.net>
2019-03-26 04:52:37 +0000

    adding makefile

---
+++ main.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+  int data;
+  struct node *next;
+};
+
+
+int Length(struct node* head)
+{
+  int count = 0;
+
+  struct node* current = head;
+
+  while(current != NULL)
+    {
+      count++;
+      current = current->next;
+    }
+
+  return count;
+}
+
+
+void Push(struct node* head, int data)
+{
+
+}
+
+
+int main()
+{
+
+  struct node* head = NULL;
+
+  head = (struct node*)malloc(sizeof(struct node));
+  head->data = 1;
+  head->next = NULL;
+
+  printf("count %d\n", Length(head));
+
+  return 0;
+}
+
+  
